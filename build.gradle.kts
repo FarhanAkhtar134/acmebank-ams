788d2bc1c255ef1b77b35712b178978c811fb93e
Farhan <farhan.akhtar@zuhlke.com>
2024-03-14 00:49:26 +0800

    chore:added logging dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.h2database:h2")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
